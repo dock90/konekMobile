c9a02fa282bcb34663014624fa91c690f9cad734
EdwardGoomba <edward@dock90.io>
2020-03-18 11:00:44 -0400

    Setup app with apollo, connect to db, display basic profile data

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,21 +1,50 @@
 import React from 'react'
-import { Button, Text, View } from 'react-native';
+import { Button, Image, Text, View } from 'react-native';
+import { useQuery } from '@apollo/client';
 import { auth } from '../config/firebase'
+// queries
+import { ME_QUERY } from '../gql/MeQuery'
 
 function ProfileScreen() {
+  const { loading, error, data } = useQuery(ME_QUERY);
+
   const handleLogout = () => {
     auth
       .signOut()
     console.log('Logout Success')
   }
 
+  if (loading) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>'Loading...'</Text>
+      </View>
+    )
+  }
+
+  if (error) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>There was an error:</Text>
+        <Text>{error.message}</Text>
+      </View>
+    )
+  }
+
+  const {
+    me: {
+      name,
+      city,
+      state,
+    }
+  } = data
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Profile Screen</Text>
+      <Text>{name}</Text>
+      <Text>{city}, {state}</Text>
       <Button
         onPress={handleLogout}
         title="Logout"
-
       />
     </View>
   );
