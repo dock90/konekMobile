23af473e847c8f5c9d1a4613746381dc264581af
EdwardGoomba <edward@dock90.io>
2020-03-31 15:56:32 -0400

    Update secondary header styles for contacts

--- screens/MessagesStackScreen.js
+++ screens/MessagesStackScreen.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack';
+import { Image, StyleSheet, TouchableOpacity } from 'react-native'
 // screens
 import MessagesScreen from './MessagesScreen'
 import MessageScreen from './MessageScreen'
 // components
 import Header from '../components/Header'
+// icons
+import group3x from '../assets/group3x.png'
 
 const MessagesStack = createStackNavigator();
 
@@ -31,10 +34,32 @@ function MessagesStackScreen() {
       />
       <MessagesStack.Screen
         name="Message"
+        options={{
+          headerStyle: {
+            height: 70
+          },
+          headerRight: () => (
+            <TouchableOpacity>
+              <Image
+                source={group3x}
+                style={styles.menu}
+              />
+            </TouchableOpacity>
+          ),
+        }}
         component={MessageScreen}
       />
     </MessagesStack.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  menu: {
+    height: 15,
+    width: 15,
+    marginRight: 20,
+    marginBottom: 20,
+  }
+})
 
 export default MessagesStackScreen
