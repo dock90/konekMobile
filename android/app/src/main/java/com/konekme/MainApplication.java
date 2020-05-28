3817106669effdf4d47e781bd07bfbbf2e8d4a76
Jeffrey Brubaker <jeffreymb@gmail.com>
2020-05-28 17:08:54 -0400

    Update to latest version of react-native

--- android/app/src/main/java/com/konekme/MainApplication.java
+++ android/app/src/main/java/com/konekme/MainApplication.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -78,7 +79,7 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    initializeFlipper(this); // Remove this line if you don't want Flipper enabled
+    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
 
     if (!BuildConfig.DEBUG) {
       UpdatesController.initialize(this);
@@ -86,18 +87,23 @@ public class MainApplication extends Application implements ReactApplication {
   }
 
   /**
-   * Loads Flipper in React Native templates.
+   * Loads Flipper in React Native templates. Call this in the onCreate method with something like
+   * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
    *
    * @param context
+   * @param reactInstanceManager
    */
-  private static void initializeFlipper(Context context) {
+  private static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
     if (BuildConfig.DEBUG) {
       try {
         /*
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.facebook.flipper.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.rndiffapp.ReactNativeFlipper");
+                aClass
+                    .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
+                    .invoke(null, context, reactInstanceManager);
         aClass.getMethod("initializeFlipper", Context.class).invoke(null, context);
       } catch (ClassNotFoundException e) {
         e.printStackTrace();
