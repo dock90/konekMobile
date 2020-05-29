507bce205d98a77ccbc5ed48094526a3d590b7fb
Jeffrey Brubaker <jeffreymb@gmail.com>
2020-05-29 15:22:12 -0400

    Production Android build successfull.

--- android/app/src/debug/java/com/konekme/ReactNativeFlipper.java
+++ android/app/src/debug/java/com/konekme/ReactNativeFlipper.java
@@ -1,10 +1,4 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
- * directory of this source tree.
- */
-package com.rndiffapp;
+package com.konekme;
 
 import android.content.Context;
 import com.facebook.flipper.android.AndroidFlipperClient;
