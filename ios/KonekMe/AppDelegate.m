0eb77509dcd6b81a3aacc8df4395a6e7fce71ef8
Jeffrey Brubaker <jeffreymb@gmail.com>
2020-06-04 15:54:27 -0400

    iOS app compiled correctly.

--- ios/KonekMe/AppDelegate.m
+++ ios/KonekMe/AppDelegate.m
@@ -1,5 +1,6 @@
 #import "AppDelegate.h"
 
+#import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
@@ -8,21 +9,22 @@
 #import <UMReactNativeAdapter/UMModuleRegistryAdapter.h>
 
 #if DEBUG
-    #import <FlipperKit/FlipperClient.h>
-    #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
-    #import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
-    #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
-    #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
-    #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
-    static void InitializeFlipper(UIApplication *application) {
-      FlipperClient *client = [FlipperClient sharedClient];
-      SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
-      [client addPlugin:[[FlipperKitLayoutPlugin alloc] initWithRootNode:application withDescriptorMapper:layoutDescriptorMapper]];
-      [client addPlugin:[[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
-      [client addPlugin:[FlipperKitReactPlugin new]];
-      [client addPlugin:[[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
-      [client start];
-    }
+//  #import <FlipperKit/FlipperClient.h>
+//  #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
+//  #import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
+//  #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
+//  #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
+//  #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+//
+//  static void InitializeFlipper(UIApplication *application) {
+//    FlipperClient *client = [FlipperClient sharedClient];
+//    SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
+//    [client addPlugin:[[FlipperKitLayoutPlugin alloc] initWithRootNode:application withDescriptorMapper:layoutDescriptorMapper]];
+//    [client addPlugin:[[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
+//    [client addPlugin:[FlipperKitReactPlugin new]];
+//    [client addPlugin:[[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
+//    [client start];
+//  }
 #endif
 
 @interface AppDelegate ()
@@ -38,7 +40,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   #if DEBUG
-    InitializeFlipper(application);
+//    InitializeFlipper(application);
   #endif
   self.moduleRegistryAdapter = [[UMModuleRegistryAdapter alloc] initWithModuleRegistryProvider:[[UMModuleRegistryProvider alloc] init]];
   self.launchOptions = launchOptions;
